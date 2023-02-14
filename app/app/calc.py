84030b1b479b911f8482beb833c1a57cc5b167f0
John Paul Babu <johnpaulbabu@gmail.com>
2023-02-14 18:52:42 +0000

    black formated

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,6 +5,7 @@ def add(number1, number2):
     """Add x and y and return result."""
     return number1 + number2
 
-def subtract(number1,number2):
+
+def subtract(number1, number2):
     """Subtract two numbers."""
-    return number1-number2
+    return number1 - number2
